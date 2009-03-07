11e80ca1039b61da1a570a750bfd82b3d3658517
rick <technoweenie@gmail.com>
2009-03-07 13:31:55 -0800

    don't automigrate when starting sinatra

--- config/seinfeld.rb
+++ config/seinfeld.rb
@@ -1,6 +1,4 @@
 DataMapper.setup(:default, ENV['DATABASE_URL'])
-DataMapper.auto_migrate!
-
 Seinfeld::User.github_login    = 'calendaraboutnothing'
 Seinfeld::User.github_password = 'xeE9LofzghzJT7bQ'
 Seinfeld::User.creation_token  = 'fXvefRp8g3QVmCkT'
