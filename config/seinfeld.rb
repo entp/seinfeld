9cae46084b6ee239585d15a95760a038e6962de0
rick <technoweenie@gmail.com>
2009-03-08 12:44:12 -0700

    remove seinfeld config

--- config/seinfeld.rb
+++
@@ -1,4 +0,0 @@
-DataMapper.setup(:default, ENV['DATABASE_URL'])
-Seinfeld::User.github_login    = 'calendaraboutnothing'
-Seinfeld::User.github_password = 'jGrhHd8sBQsffaDNjwroMftDZ2MYRK'
-Seinfeld::User.creation_token  = 'BgDAdmtYbjks6N3ycNxgHpiyrUpLps'
