950a69473e6892bc1844e8a9e2629406e2fda55a
Elijah Miller <elijah.miller@gmail.com>
2009-04-03 21:53:30 +0800

    Defaulting to sqlite3 for easier development.
    Signed-off-by: rick <technoweenie@gmail.com>

--- config/seinfeld.default.rb
+++ config/seinfeld.default.rb
@@ -1,4 +1,6 @@
-DataMapper.setup(:default, 'mysql://localhost/seinfeld')
+SINATRA_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+DataMapper.setup(:default, "sqlite3://#{SINATRA_ROOT}/development.sqlite3")
 
 Seinfeld::User.github_login    = ''
 Seinfeld::User.github_password = ''
